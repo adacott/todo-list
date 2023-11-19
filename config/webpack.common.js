41d823373d11458e56604652e49da5be8454e5bf
Adam <adamryancottrill@gmail.com>
2023-11-19 14:16:08 -0500

    Modified webpack config files

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -12,24 +12,28 @@ module.exports = {
     },
     module: {
         rules: [
-            { // CSS files/SASS files and style files
+            {
                 test: /\.s[ac]ss$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', {
                     loader: "sass-loader",
                     options: {
                         implementation: require("sass"),
                         sassOptions: {
-                            fiber: false, // or "fiber: require("fibers")
+                            fiber: false,
                         }
                     }
                 }]
             },
-            { // Images
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
-            { // Fonts -- should be "my-font-name.woff, my-font.woff2" and so on
+            {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(pdf|doc|docx)$/i,
                 type: 'asset/resource',
             },
         ],
@@ -38,11 +42,15 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/html/index.html",
             inject: 'body',
+            minify: {
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+                removeComments: true
+            }
         }),
         new MiniCssExtractPlugin({
             filename: "style.css", // can also do [name].css if serving up multiple files to generate the appropriate name
             chunkFilename: "[id].css"
         })
     ],
-
 };
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,38 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', {
-                    loader: "sass-loader",
-                    options: {
-                        implementation: require("sass"),
-                        sassOptions: {
-                            fiber: false, // or "fiber: require("fibers")
-                        }
-                    }
-                }]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/html/index.html",
-            inject: 'body',
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-            },
-        }),
-        new MiniCssExtractPlugin({
-            filename: "style.css",
-            chunkFilename: "[id].css"
-        })
-    ],
+
 });
